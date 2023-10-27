8f79e45057cacdb2bff414f5fe7442147973c6c5
tpmabrooq <119915706+tpmabrooq@users.noreply.github.com>
2023-10-27 10:15:58 +0530

    first  commit

---
+++ src/Products.jsx
@@ -0,0 +1,42 @@
+import product1 from "./assets/1.png"
+import product2 from "./assets/2.png"
+import product3 from "./assets/3.png"
+import product4 from "./assets/4.png"
+import product5 from "./assets/5.png"
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "IPhone",
+        price:999,
+        productImage:product1,
+    },
+
+    {
+        id:2,
+        productName: "Macbook Pro 2022 (M1)",
+        price:1999,
+        productImage:product2,
+    },
+
+    {
+        id:3,
+        productName: "Cannon M50 Camera",
+        price:699,
+        productImage:product3,
+    },
+
+    {
+        id:4,
+        productName: "WLS Van Gogh Denim Jacket",
+        price:228,
+        productImage:product4,
+    },
+
+    {
+        id:5,
+        productName: "LED Light Strips",
+        price:19,
+        productImage:product5,
+    },
+]
